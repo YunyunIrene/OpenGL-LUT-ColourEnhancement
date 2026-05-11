8ec41eab502ebb5d8b43c9d5fd228e40e8a79453
liutianqi <Tianqi1_Liu@SAIC-GM.com>
2026-05-11 15:11:47 +0800

    Add code

---
+++ build/CMakeFiles/colourenhancement.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colourenhancement.dir/icelut_opengl_demo.cpp.o"
+  "CMakeFiles/colourenhancement.dir/icelut_opengl_demo.cpp.o.d"
+  "CMakeFiles/colourenhancement.dir/main.cpp.o"
+  "CMakeFiles/colourenhancement.dir/main.cpp.o.d"
+  "colourenhancement"
+  "colourenhancement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colourenhancement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
